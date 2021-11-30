c77a9913c48cd2dcb93b6ca4305394f8c01bcbd5
Gift-Phutatham <phutathamgift@gmail.com>
2021-11-30 10:36:21 +0700

    spawn balls since the beginning of the simulation

--- hourglass.py
+++ hourglass.py
@@ -13,11 +13,13 @@ class HourGlass(object):
 
         pygame.init()
         self.screen_size = 750
+
         self.one_quarter = self.screen_size/4
         self.between_quarters1 = 1.75*self.screen_size/4
         self.one_half = self.screen_size/2
         self.between_quarters2 = 2.25*self.screen_size/4
         self.three_quarter = 3*self.screen_size/4
+
         self.screen = pygame.display.set_mode(
             (self.screen_size, self.screen_size))
         self.clock = pygame.time.Clock()
@@ -25,20 +27,19 @@ class HourGlass(object):
         self.add_glass_component()
         self.balls = []
         self.running = True
-        self.ticks_to_next_ball = 10
 
     def run(self):
+        for _ in range(5):
+            self.create_ball()
         while self.running:
-            for _ in range(self.steps_per_frame):
-                self.space.step(self.dt)
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        self.running = False
-                self.screen.fill((217, 217, 217))
-                self.space.debug_draw(self.draw_options)
-                self.update_balls()
-                pygame.display.flip()
-                self.clock.tick(50)
+            self.space.step(self.dt)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            self.screen.fill((31, 40, 57))
+            self.space.debug_draw(self.draw_options)
+            pygame.display.flip()
+            self.clock.tick(50)
 
     def add_glass_component(self):
         radius = 2
@@ -64,23 +65,18 @@ class HourGlass(object):
 
     def create_ball(self):
         mass = 10
-        radius = 15
+        radius = 10
         inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
         body = pymunk.Body(mass, inertia)
-        x = random.randint(int(self.one_quarter+radius), int(self.three_quarter-radius))
+        x = random.randint(int(self.one_quarter+radius),
+                           int(self.three_quarter-radius))
         y = self.one_quarter + radius
         body.position = x, y
         shape = pymunk.Circle(body, radius, (0, 0))
         shape.elasticity = 0.5
-        shape.friction = 0.9
+        shape.friction = 1000
         self.space.add(body, shape)
         self.balls.append(shape)
-
-    def update_balls(self):
-        self.ticks_to_next_ball -= 1
-        if self.ticks_to_next_ball <= 0:
-            self.create_ball()
-            self.ticks_to_next_ball = 100
 
 
 hour_glass = HourGlass()
